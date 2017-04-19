cf2e960a006c10c43ad86d68ded6ae2f5d61e819
pscottdevos <scott.devos@sykes.com>
2017-04-19 17:58:49 -0500

    get 'er working!

--- sqlite3python.c
+++ sqlite3python.c
@@ -4,85 +4,98 @@
  * Based on embedding code at https://docs.python.org/2/extending/embedding.html
  */
 
-#include <assert.h>
 #include <Python.h>
 #include <sqlite3ext.h>
+#include <stdlib.h>
 #include <string.h>
+SQLITE_EXTENSION_INIT1
 
-static void call_python(sqlite3_context *context, int argc, sqlite3_value **argv)
-{
-    PyObject *pName, *pModule, *pDict, *pFunc;
-    PyObject *pArgs, *pValue;
-    int i;
+static void call_python(
+  sqlite3_context *context, int argc, sqlite3_value **argv
+) {
+    PyObject *pArgs, *pFormat, *pName, *pPath, *pModule, *pFunc;
+    PyObject *pValue;
     
-    assert(argc == 3);
-
-    if (argc < 3) {
-        fprintf(stderr,"Usage: call pythonfile funcname string_arg\n");
-        return 1;
+    if (argc != 3) {
+        sqlite3_result_error(
+          context, "Usage: call pythonfile funcname string_arg\n", -1
+        );
+        return;
     }
 
-    Py_Initialize();
-    pName = PyString_FromString(argv[0]);
+    pName = PyString_FromString((char *)(void *)sqlite3_value_text(argv[0]));
+    #ifdef PYTHONPATH
+      pPath = PyString_FromString(PYTHONPATH);
+      pFormat = PyString_FromString("%s.%s");
+      pArgs = PyTuple_New(2);
+      PyTuple_SetItem(pArgs, 0, pPath);
+      PyTuple_SetItem(pArgs, 1, pName);
+      pName = PyString_Format(PyString_FromString("%s.%s"), pArgs);
+      Py_DECREF(pPath);
+      Py_DECREF(pFormat);
+      Py_DECREF(pArgs);
+    #endif
+
     /* Error checking of pName left out */
 
     pModule = PyImport_Import(pName);
     Py_DECREF(pName);
 
     if (pModule != NULL) {
-        pFunc = PyObject_GetAttrString(pModule, sqlite3_value_text(argv[1]));
+        pFunc = PyObject_GetAttrString(
+            pModule, (char *)(void *)sqlite3_value_text(argv[1])
+        );
         /* pFunc is a new reference */
 
         if (pFunc && PyCallable_Check(pFunc)) {
-            pArgs = PyTuple_New(argc - 3);
-            for (i = 2; i < argc; ++i) {
-                pValue = PyString_FromString(sqlite3_value_text(argv[i])));
-                if (!pValue) {
-                    Py_DECREF(pArgs);
-                    Py_DECREF(pModule);
-                    fprintf(stderr, "Cannot convert argument\n");
-                    return 2;
-                }
-                /* pValue reference stolen here: */
-                PyTuple_SetItem(pArgs, i - 2, pValue);
+            pArgs = PyTuple_New(1);
+            pValue = PyString_FromString(
+              (char *)(void *)sqlite3_value_text(argv[2])
+            );
+            if (!pValue) {
+                Py_DECREF(pArgs);
+                Py_DECREF(pModule);
+                sqlite3_result_error(context, "Cannot convert argument\n", -1);
+                return;
             }
+            /* pValue reference stolen here: */
+            PyTuple_SetItem(pArgs, 0, pValue);
             pValue = PyObject_CallObject(pFunc, pArgs);
             Py_DECREF(pArgs);
             if (pValue != NULL) {
-                printf("Result of call: %s\n", PyString_Asstring(pValue));
+                const char *cValue = PyString_AsString(pValue);
+                sqlite3_result_text(context, cValue, -1, NULL);
                 Py_DECREF(pValue);
-            }
-            else {
+            } else {
                 Py_DECREF(pFunc);
                 Py_DECREF(pModule);
                 PyErr_Print();
-                fprintf(stderr,"Call failed\n");
-                return 3;
+                sqlite3_result_error(context, "Call Failed\n", -1);
+                return;
             }
-        }
-        else {
+        } else {
             if (PyErr_Occurred())
                 PyErr_Print();
-            fprintf(stderr, "Cannot find function \"%s\"\n", argv[2]);
+            sqlite3_result_error(context, "Cannot find function\n", -1);
         }
         Py_XDECREF(pFunc);
         Py_DECREF(pModule);
     }
     else {
         PyErr_Print();
-        fprintf(stderr, "Failed to load \"%s\"\n", argv[1]);
-        return 4;
+        sqlite3_result_error(context, "Failed to load file\n", -1);
+        return;
     }
-    Py_Finalize();
-    return 0;
+    return;
 }
 
-int sqlite3_sqlite3python_init(
+int sqlite3_sqlitepython_init(
 sqlite3 *db, char **pzErrMsg, const sqlite3_api_routines *pApi
 ) {
   SQLITE_EXTENSION_INIT2(pApi)
   sqlite3_create_function(
     db, "CALL_PYTHON", 3, SQLITE_UTF8, NULL, call_python, NULL, NULL
   );
+  Py_Initialize();
   return 0;
 }
